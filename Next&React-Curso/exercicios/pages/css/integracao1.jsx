009ad0c182863fbd14701dccd78ebd85765df796
Edinelson Lima <edinelsonlima86@gmail.com>
2021-07-10 19:33:58 -0300

    css global

---
+++ Next&React-Curso/exercicios/pages/css/integracao1.jsx
@@ -0,0 +1,10 @@
+
+export default function integracao(){
+    return (
+        <div className="integracao">
+            <div className="vermelha">Texto #01</div>
+            <div className="azul">Texto #02</div>
+            <div className="branca">Texto #03</div>
+        </div>
+    )
+}
